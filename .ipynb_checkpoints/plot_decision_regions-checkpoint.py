4245db869beac00dcec55589a31f9e233e9ec703
alextanhongpin <alextan220990@gmail.com>
2022-01-16 00:20:33 +0800

    chore: add chapter 3

--- .ipynb_checkpoints/plot_decision_regions-checkpoint.py
+++ .ipynb_checkpoints/plot_decision_regions-checkpoint.py
@@ -33,3 +33,50 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
             label=c1,
             facecolor="black",
         )
+
+        
+        
+def plot_decision_regions2(X, y, classifier, test_idx=None, resolution=0.02):
+    # Setup marker generator and color map
+    markers = list("sxo^v")
+    colors = ("red", "blue", "lightgreen", "gray", "cyan")
+    cmap = ListedColormap(colors[: len(np.unique(y))])
+
+    # Plot the decision surfaces.
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(
+        np.arange(x1_min, x1_max, resolution),
+        np.arange(x2_min, x2_max, resolution),
+    )
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(
+            x=X[y == cl, 0],
+            y=X[y == cl, 1],
+            alpha=0.8,
+            c=colors[idx],
+            marker=markers[idx],
+            label=cl,
+            edgecolor="black",
+        )
+    # Highlight test sample
+    if test_idx:
+        # Plot all samples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(
+            X_test[:, 0],
+            X_test[:, 1],
+            c=None,
+            edgecolor="black",
+            alpha=1.0,
+            linewidth=1,
+            marker="o",
+            s=100,
+            label="test set",
+        )
