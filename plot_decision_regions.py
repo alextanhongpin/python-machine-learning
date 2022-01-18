58acdfd05c8bc26b0b0bd5c87416b44f95949519
alextanhongpin <alextan220990@gmail.com>
2022-01-19 00:18:36 +0800

    chore: cleanup jupyter notebook solutions

--- plot_decision_regions.py
+++ plot_decision_regions.py
@@ -4,39 +4,7 @@ from matplotlib.colors import ListedColormap
 
 
 # Visualizing the decision boundaries of the two-dimensional dataset.
-def plot_decision_regions(X, y, classifier, resolution=0.02):
-    # setup marker generator and color map
-    markers = list("sxo^v")
-    colors = ("red", "blue", "lightgreen", "gray", "cyan")
-    cmap = ListedColormap(colors[: len(np.unique(y))])
-
-    # Plot the decision surface.
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(
-        np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution)
-    )
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    # Plot class samples.
-    for idx, c1 in enumerate(np.unique(y)):
-        plt.scatter(
-            x=X[y == c1, 0],
-            y=X[y == c1, 1],
-            alpha=0.8,
-            c=colors[idx],
-            marker=markers[idx],
-            label=c1,
-            facecolor="black",
-        )
-
-        
-        
-def plot_decision_regions2(X, y, classifier, test_idx=None, resolution=0.02):
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     # Setup marker generator and color map
     markers = list("sxo^v")
     colors = ("red", "blue", "lightgreen", "gray", "cyan")
@@ -63,7 +31,7 @@ def plot_decision_regions2(X, y, classifier, test_idx=None, resolution=0.02):
             c=colors[idx],
             marker=markers[idx],
             label=cl,
-            edgecolor="black",
+            facecolor="black",
         )
     # Highlight test sample
     if test_idx:
@@ -72,9 +40,8 @@ def plot_decision_regions2(X, y, classifier, test_idx=None, resolution=0.02):
         plt.scatter(
             X_test[:, 0],
             X_test[:, 1],
-            c=None,
-            edgecolor="black",
-            alpha=1.0,
+            edgecolor='blue',
+            alpha=0.5,
             linewidth=1,
             marker="o",
             s=100,
